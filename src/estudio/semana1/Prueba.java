0d66882525a3122f867ce885fcc74b42e6f0205a
Jennifer <jennifer.natis@gmail.com>
2020-10-30 18:08:50 -0500

    mi primera version, los proyectos tienen carpetas llamadas package y que las clases deben empezar en Mayuscula

---
+++ src/estudio/semana1/Prueba.java
@@ -0,0 +1,11 @@
+package estudio.semana1;
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mi primera prueba");
+
+	}
+
+}
