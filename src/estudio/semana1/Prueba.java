6fc58d4b4bdc2e84a448fb4954b0ebcf7ecd2132
jennifernatis <jennifer.natis@gmail.com>
2020-11-11 18:04:41 -0500

    ejercicio tipos de datos

--- src/estudio/semana1/Prueba.java
+++ src/estudio/semana1/Prueba.java
@@ -1,10 +1,44 @@
 package estudio.semana1;
-public class Prueba {
 
+import java.math.BigDecimal;
+
+public class Prueba {
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Mi primera prueba dos");
+		
+		
+//      Java es tipado
+//		int
+//		long
+//		String
+//		boolean
+//		double
+		
+		
+		float cantidad1 = 1;
+		float cantidad2 = 3;
+		BigDecimal plata;
+		
+		//error java no es debilmente tipado no le puedo cambiar el tipo.
+		//cantidad1="hola";
+		
+		
+		System.out.println(cantidad1/cantidad2);
+		
+//		// javascript no es tipado
+////		let cantidad1=1   (let cree una variable nueva) java script
+//		let cantidad2='Hola mundo'
+//		let cantidad3=true
+//		cantidad2 = false
+//		
+		
+		
+		
+		
 
 	}
 
