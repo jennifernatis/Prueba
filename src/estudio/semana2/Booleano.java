5eb9f80ba2f2cb3c3c360ce5d95d7f961d6eb056
jennifernatis <jennifer.natis@gmail.com>
2020-11-18 22:08:12 -0500

    explicación semi detallada de los operadores

--- src/estudio/semana2/Booleano.java
+++ src/estudio/semana2/Booleano.java
@@ -8,8 +8,8 @@ public class Booleano {
 		// Programa que autoriza o no la salida de la house escenario : a) solo sale si
 		// hizo todos los oficios
 
-		Boolean lave_platos = false;
-		Boolean trapie_piso = false;
+		Boolean lave_platos = true;
+		Boolean trapie_piso = true;
 		Boolean limpiar_arenera = null;
 
 		// Null asigna un valor que no genere errores en operaciones antes de asignar un
@@ -17,11 +17,11 @@ public class Booleano {
 
 		// operaciones booleanas
 		// And -> y los dos son verdaderos
-		// Or -> o
+		// Or -> o uno es verdadero
 		// not -> no
 		// xor -> o exclusivo
 
-		// OPERACIONES AND
+		// OPERACIONES AND  devuelve true si ambos operandos son true, evaluándolos ambos.
 
 //		if (lave_platos & trapie_piso) {
 //			System.out.println("puede salir ");
@@ -29,17 +29,26 @@ public class Booleano {
 //			System.out.println(" No me sale !!!");
 //		}
 
-		
-		
-		// OPERACIONES OR
+		// OPERACIONES OR devuelve true uno de los operandos es true, evaluándolos ambos.
 
 		// Programa que autoriza o no la salida de la house escenario : a) solo sale si
-				// hizo algun oficio
-		
-		if (lave_platos | limpiar_arenera) {
+		// hizo algun oficio
+
+//		if (lave_platos | limpiar_arenera) {
+//			System.out.println("puede salir ");
+//		} else {
+//			System.out.println(" No me sale !!!");
+
+		// tarea explicar la diferencia entre | y ||
+		// subir a git hecho
+
+		// OPERACIONES XOR devuelve true si alguno de los operandos son true.
+
+		if (lave_platos || trapie_piso) {
 			System.out.println("puede salir ");
 		} else {
 			System.out.println(" No me sale !!!");
+
 		}
 
 	}
